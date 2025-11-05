01bb5de0e741a3ea75c193987fb1a389be757853
Ricardo Alamo Salgado <126198539+richardalamo@users.noreply.github.com>
2025-11-05 08:01:11 -0500

    test

--- test.py
+++ test.py
@@ -6,3 +6,10 @@ def is_prime(n: int) -> bool:
         return True
     if n % 2 == 0 or n % 3 == 0:
         return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
